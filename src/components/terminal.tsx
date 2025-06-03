2d2537c7944e50185a081e74bd56b84333f8b13f
rhysm <rhys.link.0919@gmail.com>
2025-06-03 15:49:30 -0500

    Reworked frontend to handle new websocket frontend

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -3,22 +3,56 @@ import { useEffect, useRef, useState } from "react"
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function Terminal() {
-    const [terminalText, setTerminalText] = useState<string>("");
-    const serverTextEvent = useRef<EventSource>(null)
+//     const [terminalText, setTerminalText] = useState<string>("");
+    const [terminalTextList, setTerminalTextList] = useState<string[]>([]);
+    const termEnd = useRef<HTMLDivElement>(null);
+    const [ws, setWs] = useState<WebSocket>();
 
     useEffect(() => {
-            serverTextEvent.current = new EventSource(`${API_BASE_URL}/api/projects/terminal/startsession`)
+        if(ws != undefined) {return}
+        const websocket = new WebSocket(`${API_BASE_URL}/api/projects/terminal/startsession`)
+        websocket.onopen = () => {
+            console.log("Websocket Connected")
+        }
+        websocket.onmessage = (event: any) => {
+            const message = (event.data);
+            console.log(message);
 
-            serverTextEvent.current.onmessage = function(event: any) {
-                console.log(event.data);
-                setTerminalText(event.data);
-            }
-    })
+            setTerminalTextList((prevText) => [...prevText, message]);
+            window.scrollTo({
+                top: document.documentElement.scrollHeight,
+                behavior: 'auto'
+            })
+        }
+        websocket.onclose = () => {
+            console.log("Console Closed")
+        }
+        websocket.onerror = function(error) {
+            console.error('WebSocket error:', error);
+        };
+        setWs(websocket);
+
+        return () => {
+            websocket.close();
+        }
+
+    }, [])
+
+    const scrollToBottom = () => {
+        termEnd.current?.scrollIntoView({behavior: 'smooth'});
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [terminalTextList]);
 
     return (
-        <>
-            {terminalText}
-        </>
+        <div>
+            {terminalTextList && (terminalTextList.map((text: string) =>
+                <div>{text}</div>
+            ))}
+            <div ref={termEnd}/>
+        </div>
     )
 
 }
